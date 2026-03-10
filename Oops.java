a79dfb7a26d6ab0f8772f76bd081c93da3c65fdc
viswanath712008 <viswanath622008@gmail.com>
2026-03-10 22:12:14 +0530

    Implement HashMap for ASCII patterns on main branch

--- Oops.java
+++ Oops.java
@@ -18,6 +18,49 @@ public class Oops {
         });
         patterns.put('P', new String[]{
             "  PPPP ",
+            "  P   P ",
+            " PPPP  ",
+            "P     ",
+            " P     ",
+            "  P     "
+        });
+        patterns.put('S', new String[]{
+            "  SSSS ",
+            "S     ",
+            "  SSS ",
+            "     S ",
+            "    SSS ",
+            "     SSS"
+        });
+    }
+
+    public static String[] getPattern(char c) {
+        return patterns.get(c);
+    }
+
+    public static void displayMessage(String message) {
+        generatePatterns();
+        for (int i = 0; i < 6; i++) {
+            for (char c : message.toCharArray()) {
+                String[] pat = getPattern(c);
+                if (pat != null && i < pat.length) {
+                    System.out.print(pat[i]);
+                }
+            }
+            System.out.println();
+        }
+
+    private static void generatePatterns() {
+        patterns.put('O', new String[]{
+            "  OOO ",
+            " O   O",
+            "O     O",
+            "O     O",
+            " O   O ",
+            "  OOO "
+        });
+        patterns.put('P', new String[]{
+            "  PPPP ",
             " P   P ",
             " PPPP  ",
             "P     ",
