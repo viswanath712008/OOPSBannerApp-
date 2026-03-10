f1f64136a6ef3382146a51ca8512986c1cb91094
viswanath712008 <viswanath622008@gmail.com>
2026-03-10 23:24:08 +0530

    fix class name and update uc1

--- Oops.java
+++ Oops.java
@@ -1,53 +1,10 @@
-import java.util.HashMap;
-
-public class Oops {
-    private static HashMap<Character, String[]> patterns = new HashMap<>();
-
+public class OopsOriginal {
     public static void main(String[] args) {
-        displayMessage("OOPS");
-    }
-
-    private static void generatePatterns() {
-        patterns.put('O', new String[]{
-            "  OOO ",
-            " O   O",
-            "O     O",
-            "O     O",
-            " O   O ",
-            "  OOO "
-        });
-        patterns.put('P', new String[]{
-            "  PPPP ",
-            " P   P ",
-            " PPPP  ",
-            "P     ",
-            " P     ",
-            "  P     "
-        });
-        patterns.put('S', new String[]{
-            "  SSSS ",
-            "S     ",
-            "  SSS ",
-            "     S ",
-            "    SSS ",
-            "     SSS"
-        });
-    }
-
-    public static String[] getPattern(char c) {
-        return patterns.get(c);
-    }
-
-    public static void displayMessage(String message) {
-        generatePatterns();
-        for (int i = 0; i < 6; i++) {
-            for (char c : message.toCharArray()) {
-                String[] pat = getPattern(c);
-                if (pat != null && i < pat.length) {
-                    System.out.print(pat[i]);
-                }
-            }
-            System.out.println();
-        }
+        System.out.println("  OOO   OOO   PPPP   SSSS ");
+        System.out.println(" O   O O   O  P   P S     ");
+        System.out.println("O     O     O PPPP   SSS  ");
+        System.out.println("O     O     O P         S ");
+        System.out.println(" O   O  O   O  P     SSS  ");
+        System.out.println("  OOO    OOO   P      SSS ");
     }
 }
---
+++ oopsuc1.java
@@ -0,0 +1,5 @@
+public class oopsuc1: {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
