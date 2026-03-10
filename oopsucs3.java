6d9ad47ced737bd9064d39a6904a46d6a0e6ff23
viswanath712008 <viswanath622008@gmail.com>
2026-03-10 23:47:22 +0530

    Add version 3.0 with String join

---
+++ oopsucs3.java
@@ -0,0 +1,15 @@
+//version 3.0
+public class oopsucs3 {
+     public static void main(String[] args) {
+    System.out.println(String.join("  ","   ***   ","   ***   ","******   ","   ***** "));
+    System.out.println(String.join("  "," **   ** "," **   ** ","**    ** "," **      "));
+    System.out.println(String.join("  ","**     **","**     **","**     **","**       "));
+    System.out.println(String.join("  ","**     **","**     **","**    ** "," **      "));
+    System.out.println(String.join("  ","**     **","**     **","******   ","   ***   "));
+    System.out.println(String.join("  ","**     **","**     **","**       ","      ** "));
+    System.out.println(String.join("  ","**     **","**     **","**       ","       **"));
+    System.out.println(String.join("  "," **   ** "," **   ** ","**       ","      ** "));
+    System.out.println(String.join("  ","   ***   ","   ***   ","**       "," *****   "));
+    System.out.println("                                                               ");
+     }
+}
