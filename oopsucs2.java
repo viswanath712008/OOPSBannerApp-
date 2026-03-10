5aa573b6ee884589b4ea32bc38688168cd507e15
viswanath712008 <viswanath622008@gmail.com>
2026-03-10 23:38:58 +0530

    Add OOPS ASCII banner file

---
+++ oopsucs2.java
@@ -0,0 +1,14 @@
+public class oopsucs2 {
+     public static void main(String[] args) {
+    System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"   ***** ");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+" **      ");
+    System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"   ***   ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"       **");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"      ** ");
+    System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+" *****   ");
+
+     }
+}
